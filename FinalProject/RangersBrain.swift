064e60485f8b561e60c5ef792315e65f1c931321
Zick <zicky.chan.95@gmail.com>
2017-12-31 19:24:45 +0100

    initial commit

--- FinalProject/RangersBrain.swift
+++ FinalProject/RangersBrain.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+
+struct rangersBrain {
+    private let rangerName = ["Gaoranger","Hurricaneger","Abaranger","Dekaranger","Magiranger","Boukenger","Gekiranger","Go-onger","Shinkenger","Goseiger","Gokaiger","Go-Busters","Kyoryuger"]
+    private let rangerSubtitle = ["Power Rangers Wild Force","Power Rangers Ninja Storm","Power Rangers Dino Thunder","Power Rangers S.P.D","Power Rangers Mystic Force","Power Rangers Operation Overdrive","Power Rangers Jungle Fury","Power Rangers RPM","Power Rangers Samurai","Power Rangers Megaforce","Power Rangers Super Megaforce","No American Version","Power Rangers Dino Charge"]
+    private let time = ["2001-2002","2002-2003","2003-2004","2004-2005","2005-2006","2006-2007","2007-2008","2008-2009","2009-2010","2010-2011","2011-2012","2012-2013","2013-2014"]
+    private let description = ["Power Rangers Wild Force, often abbreviated as \"PRWF\", was the tenth incarnation of the Power Rangers series, based on Hyakujuu Sentai Gaoranger. Wild Force, like Time Force, is very faithful to its Sentai counterpart, with nearly the exact same plot. It's also the first season to have the exact same morphing sequence as its Sentai counterpart."]
+    
+    let rangerLength = 13
+    
+    func getDetails(index : Int) ->[String]{
+        return [rangerName[index],rangerSubtitle[index]]
+    }
+    
+    func getFullDetails(index : Int) ->[String]{
+        return [rangerName[index],rangerSubtitle[index],time[index],description[0]]
+    }
+}
--- FinalProject/TableViewController.swift
+++ FinalProject/TableViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
+    
+    private let brain: rangersBrain = rangersBrain();
+    var passingValue: [String] = ["","","",""];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -29,23 +32,27 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return brain.rangerLength
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
 
+        let arr = brain.getDetails(index: indexPath.row)
+        
         // Configure the cell...
-
+        cell.textLabel?.text = arr[0]
+        cell.detailTextLabel?.text = arr[1]
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -81,15 +88,27 @@ class TableViewController: UITableViewController {
         return true
     }
     */
-
-    /*
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //Get the passing value details
+        passingValue = brain.getFullDetails(index: indexPath.row)
+        print("Select \(indexPath.row)")
+        
+        self.performSegue(withIdentifier: "showDetail", sender: self)
+    }
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        // initialize new view controller and cast it as your view controller
+        let viewController = segue.destination as! ViewController;
+        // your new view controller should have property that will store passed value
+        viewController.passedValue = passingValue;
     }
-    */
+    
 
 }
